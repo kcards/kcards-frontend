2cf5bf8c200e919ec672042460de06a9781a5f8a
Kevin Greene <kevindgreene@gmail.com>
2017-07-25 08:21:01 -0400

    Updated styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import logo from "./logo.svg";
+import logo from "./k_card.png";
 import "./App.css";
 import RoomPrompt from "./RoomPrompt";
 import NamePrompt from "./NamePrompt";
--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,23 @@
 import React, {Component} from "react";
+import "./Card.css"
+
+
+let colorMap = {
+    '#018E42': "green",
+    '#F7D002': "yellow",
+    '#BF1A2F': "red",
+
+};
+
+
 export const Card = ({name, color, big}) => {
-    let bigDisplay = big ? "This is big" : "Not big";
+    let bigDisplay = big ? "big-card jumbotron text-center" : "little-card";
+    let colorClass = colorMap[color] || color
     return (
-        <div className="card" style={{color: color}}>
-            {bigDisplay}
-            {name} - {color}
+        <div className={"card " + colorClass + " " + bigDisplay}>
+            {name}
         </div>
     )
-}
+};
+
 export default Card;
